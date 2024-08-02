01d81c20b56817b9f04a47c67d9a9843d64e5243
lunaro-4 <pavelike@gmail.com>
2024-08-02 20:09:58 +0300

    feat(gtk-app): add sample handcrafted gtk app

--- main.c
+++ main.c
@@ -4,9 +4,22 @@
 #include <stdio.h>
 /* more stuff here */
 
+static gboolean on_drop (GtkDropTarget *target){
+	return TRUE;
+}
+
 static void app_main(GtkApplication *app, gpointer user_data)
 { 
-	return;
+	GtkWidget *window;
+	window = gtk_application_window_new (app);
+	
+	gtk_window_set_title (GTK_WINDOW (window), "test");
+	gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+	gtk_window_present(GTK_WINDOW(window));
+
+	GtkWidget *drag_and_drop;
+	gtk_drop_target_new(G_TYPE_INVALID, GDK_ACTION_COPY);
+	
 }
 
 static void window_main(GtkWidget *window, gpointer user_data)
@@ -20,13 +33,13 @@ int main(int argc, char *argv[])
 	int status;
 
 	app = gtk_application_new("org.application.me", G_APPLICATION_DEFAULT_FLAGS);
-	gtk_init();
-	GtkWidget *window;
-	window = gtk_window_new();
-	gtk_widget_set_visible(window, true);
+	// GtkWidget *window;
+	// window = gtk_window_new();
+	// gtk_widget_set_visible(window, true);
+
+	// gtk_application_add_window(app, window);
 
 	g_signal_connect(app, "activate", G_CALLBACK(app_main), NULL);
-
 	status = g_application_run(G_APPLICATION(app), argc, argv);
 
 	return status;
