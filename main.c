f8a12f20c116255a4901c10a9172c9944c20df77
lunaro-4 <pavelike@gmail.com>
2024-08-04 20:46:59 +0300

    WIP

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+// #include <png.h>
 
 int main(int argc, char *argv[])
 {
+	void *vo;
+	int c;
+	FILE *file = fopen("in.png", "rb");
+	while ((c = fread(vo, unsigned long, unsigned long, FILE *)) != EOF){
+	}
 	return 0;
 }
