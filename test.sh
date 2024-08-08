124eb2a07a06f72507d2e96d25ec22b16359af94
lunaro-4 <pavelike@gmail.com>
2024-08-08 20:32:30 +0300

    test(test.sh): improve autotest script to launch valgrind only on errors, not fails

--- test.sh
+++ test.sh
@@ -1,3 +1,15 @@
 #!/bin/env bash
 
-make tests && make test
+make tests
+
+MAKE_TEST_OUTP=`make test`
+N_OF_ERRORS=$(echo "$MAKE_TEST_OUTP" | grep "Errors:"  | sed 's/^\(.*\)://' | sed -z 's/ //g' | awk '{print}' ORS=' ' | sed 's/ / + /')
+
+# echo "$MAKE_TEST_OUTP"
+if [ $((N_OF_ERRORS)) -ge 1 ]
+then 
+	valgrind out/tests all
+else
+	echo "$MAKE_TEST_OUTP"
+fi
+
